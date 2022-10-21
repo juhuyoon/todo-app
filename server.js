2e31e62bc995d6857ece8eb7b0b6d3313ed02405
Junghoon Yoon <jyoon3@2u.com>
2022-10-21 14:01:36 -0400

    index.js for models done

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const sequelize = require("./config/connection");
 
+const Model = require("./models");
+
 const PORT = 5500;
 
 sequelize.sync().then(() => {
